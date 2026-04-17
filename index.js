41c3029a06e2522a7870ad7006c5b9f9da5b4d05
ponmanivasahan <vasupks01@gmail.com>
2026-04-17 17:28:25 +0530

    project setup done

---
+++ index.js
@@ -0,0 +1,7 @@
+const canvas=document.querySelector('canvas')
+const c=canvas.getContent('2d')
+
+canvas.width=1024
+canvas.height=576
+c.fillStlyle='white'
+c.fillRect(0,0,canvas.width,canvas.height);
