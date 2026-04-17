4c2e62702d664797a41e9decd4f3739b8bd47f42
ponmanivasahan <vasupks01@gmail.com>
2026-04-17 20:49:03 +0530

    Worked on collision block detecting the collisions

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const canvas=document.querySelector('canvas')
 const c=canvas.getContext('2d')
 
-canvas.width=1720
-canvas.height=698
+canvas.width=1024
+canvas.height=576
+
+const parsedCollisions=collisionLevel1.parse2D()
+const CollisionBlocks=parsedCollisions.createObjectsFrom2D()
+
+const backgroundLevel1=new Sprite({
+    position:{x:0,y:0},
+    imageSrc:'./sprites/bg.png',
+})
 
 const player=new Player()
 
@@ -19,8 +27,12 @@ const keys={
 }
 function animate(){
     window.requestAnimationFrame(animate)
-    c.fillStyle='white'
-    c.fillRect(0,0,canvas.width,canvas.height);
+    // c.fillStyle='white'
+    // c.fillRect(0,0,canvas.width,canvas.height);
+    backgroundLevel1.draw()
+    CollisionBlocks.forEach((CollisionBlock)=>{
+        CollisionBlock.draw()
+    })
     player.velocity.x=0
     if(keys.d.pressed){
         player.velocity.x=5
