a149c56871f00f3da3f5b2c5a82bc91b67acb1d9
ponmanivasahan <vasupks01@gmail.com>
2026-04-17 18:38:08 +0530

    I was working on adding the gravity

--- Player.js
+++ Player.js
@@ -4,11 +4,18 @@ class Player{
             x:100,
             y:100,
         }
+       
+         this.velocity={
+            x:0,
+            y:0,
+         }
+        
         this.width=100
         this.height=100
         this.sides={
             bottom:this.position.y + this.height,
         }
+        this.gravity=1
     }
 
     draw(){
@@ -16,9 +23,13 @@ class Player{
         c.fillRect(this.position.x,this.position.y,this.width,this.height)
     }
     update(){
-        if(this.sides.bottom<canvas.height){
-            this.position.y++
+        this.position.y+=this.velocity.y
+        if(this.sides.bottom + this.velocity.y<canvas.height){
+            this.velocity.y+=this.gravity
             this.sides.bottom=this.position.y+this.height
+        }
+        else{
+            this.velocity.y=0
         }
     }
 }
